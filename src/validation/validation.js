95bc687c306e66f30a103ec3b7fca1d631f0f12c
Derese92 <54817093+Derese92@users.noreply.github.com>
2020-04-03 17:00:15 -0500

    practice

---
+++ src/validation/validation.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const validation = (props) => {
+let validationMessage = 'TExt long enough';
+
+if (props.inputLength <= 5){
+    validationMessage = 'Text too short';
+}
+
+return(
+    <div>
+        <p>{validationMessage}</p>
+    </div>
+    
+);
+};
+
+export default validation;
